f750bf0a2cd5543cd6c30c878a9eea94894809c0
João da Mata <jlfilho@uea.edu.br>
2021-11-29 22:09:28 -0400

    Learning transfer done

--- models/espcn/model_espcn.py
+++ models/espcn/model_espcn.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from models.Model import Model
 
-def espcn(scale_factor=2,file_writer_cm=None):   
+def espcn(channels=1,scale_factor=2,file_writer_cm=None):   
     inputs = tf.keras.layers.Input(shape=(None,None,1),name='input')
 
     net = tf.pad(inputs, [[0, 0], [4, 4], [4, 4], [0, 0]], 'SYMMETRIC')
@@ -13,7 +13,7 @@ def espcn(scale_factor=2,file_writer_cm=None):
     net = tf.keras.layers.Conv2D(32, 3, padding='valid',strides=(1, 1), name='conv2',
                                 kernel_initializer=tf.keras.initializers.he_normal())(net)
     net = tf.keras.activations.tanh(net)
-    net = tf.keras.layers.Conv2D(scale_factor ** 2, 3,padding='valid',strides=(1, 1), name='final',
+    net = tf.keras.layers.Conv2D(channels*(scale_factor ** 2), 3,padding='valid',strides=(1, 1), name='final',
                                 kernel_initializer=tf.keras.initializers.he_normal())(net)
     net = tf.keras.layers.Lambda(lambda x:tf.nn.depth_to_space(x,scale_factor),name = 'prediction')(net)
     outputs = tf.keras.activations.sigmoid(net)
--- run_train.sh
+++ run_train.sh
@@ -1,40 +1,82 @@
 #!/bin/bash
 
-#2x
-python train.py --model espcn --batch_size 64 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 100 --steps_per_epochs 100 --epochs_per_save 5
-#4x
-python train.py --model espcn --train_dataset_info_path datasets/train/dataset_info.txt --train_dataset_path datasets/train/dataset.tfrecords --valid_dataset_info_path datasets/test/dataset_info.txt --valid_dataset_path datasets/test/dataset.tfrecords --batch_size 64 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 100 --steps_per_epochs 100 --epochs_per_save 5
-
-
+# ESPCN Train 2x
+python train.py --model espcn --train_dataset_info_path datasets/train/2x/dataset_info.txt --train_dataset_path datasets/train/2x/dataset.tfrecords --valid_dataset_info_path datasets/test/2x/dataset_info.txt --valid_dataset_path datasets/test/2x/dataset.tfrecords --batch_size 32 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5
+sleep 10
+# ESPCN Train 2x, load weights, lr 1e-4
+python train.py --model espcn --train_dataset_info_path datasets/train/2x/dataset_info.txt --train_dataset_path datasets/train/2x/dataset.tfrecords --valid_dataset_info_path datasets/test/2x/dataset_info.txt --valid_dataset_path datasets/test/2x/dataset.tfrecords --batch_size 32 --learning_rate 1e-4 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --load_weights
+sleep 10
+# ESPCN 4x, transfer learning, lr 10e-3 
+python train.py --model espcn --train_dataset_info_path datasets/train/4x/dataset_info.txt --train_dataset_path datasets/train/4x/dataset.tfrecords --valid_dataset_info_path datasets/test/4x/dataset_info.txt --valid_dataset_path datasets/test/4x/dataset.tfrecords --batch_size 32 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --transfer_learning
+sleep 10
+# ESPCN 4x, load weights, lr 10e-4
+python train.py --model espcn --train_dataset_info_path datasets/train/4x/dataset_info.txt --train_dataset_path datasets/train/4x/dataset.tfrecords --valid_dataset_info_path datasets/test/4x/dataset_info.txt --valid_dataset_path datasets/test/4x/dataset.tfrecords --batch_size 32 --learning_rate 1e-4 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --load_weights
 sleep 10
 
-python train.py --model g_rtsrgan --batch_size 32 --learning_rate 1e-2 --lr_decay_epochs 10 --num_epochs 100 --steps_per_epochs 100 --epochs_per_save 5
-
-python train.py --model g_rtsrgan --batch_size 64 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 100 --steps_per_epochs 100 --epochs_per_save 5
-
-#4x
-python train.py --model g_rtsrgan --train_dataset_info_path datasets/train/dataset_info.txt --train_dataset_path datasets/train/dataset.tfrecords --valid_dataset_info_path datasets/test/dataset_info.txt --valid_dataset_path datasets/test/dataset.tfrecords --batch_size 64 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 100 --steps_per_epochs 100 --epochs_per_save 5
-
-
+# G_RTRSGAN Train 2x
+python train.py --model g_rtsrgan --train_dataset_info_path datasets/train/2x/dataset_info.txt --train_dataset_path datasets/train/2x/dataset.tfrecords --valid_dataset_info_path datasets/test/2x/dataset_info.txt --valid_dataset_path datasets/test/2x/dataset.tfrecords --batch_size 32 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5
+sleep 10
+# G_RTRSGAN Train 2x, load weights, lr 1e-4
+python train.py --model g_rtsrgan --train_dataset_info_path datasets/train/2x/dataset_info.txt --train_dataset_path datasets/train/2x/dataset.tfrecords --valid_dataset_info_path datasets/test/2x/dataset_info.txt --valid_dataset_path datasets/test/2x/dataset.tfrecords --batch_size 32 --learning_rate 1e-4 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --load_weights
+sleep 10
+# G_RTRSGAN 4x, transfer learning, lr 10e-3 
+python train.py --model g_rtsrgan --train_dataset_info_path datasets/train/4x/dataset_info.txt --train_dataset_path datasets/train/4x/dataset.tfrecords --valid_dataset_info_path datasets/test/4x/dataset_info.txt --valid_dataset_path datasets/test/4x/dataset.tfrecords --batch_size 32 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --transfer_learning
+sleep 10
+# G_RTRSGAN 4x, load weights, lr 10e-4
+python train.py --model g_rtsrgan --train_dataset_info_path datasets/train/4x/dataset_info.txt --train_dataset_path datasets/train/4x/dataset.tfrecords --valid_dataset_info_path datasets/test/4x/dataset_info.txt --valid_dataset_path datasets/test/4x/dataset.tfrecords --batch_size 32 --learning_rate 1e-4 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --load_weights
 sleep 10
 
-python train.py --model g_ertsrgan --batch_size 32 --learning_rate 1e-2 --lr_decay_epochs 10 --num_epochs 100 --steps_per_epochs 100 --epochs_per_save 5
-
-#4x
-python train.py --model g_ertsrgan --train_dataset_info_path datasets/train/dataset_info.txt --train_dataset_path datasets/train/dataset.tfrecords --valid_dataset_info_path datasets/test/dataset_info.txt --valid_dataset_path datasets/test/dataset.tfrecords --batch_size 64 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 100 --steps_per_epochs 100 --epochs_per_save 5
-
-
+# G_ERTSRGAN Train 2x
+python train.py --model g_ertsrgan --train_dataset_info_path datasets/train/2x/dataset_info.txt --train_dataset_path datasets/train/2x/dataset.tfrecords --valid_dataset_info_path datasets/test/2x/dataset_info.txt --valid_dataset_path datasets/test/2x/dataset.tfrecords --batch_size 32 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5
+sleep 10
+# G_ERTSRGAN Train 2x, load weights, lr 1e-4
+python train.py --model g_ertsrgan --train_dataset_info_path datasets/train/2x/dataset_info.txt --train_dataset_path datasets/train/2x/dataset.tfrecords --valid_dataset_info_path datasets/test/2x/dataset_info.txt --valid_dataset_path datasets/test/2x/dataset.tfrecords --batch_size 32 --learning_rate 1e-4 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --load_weights
 sleep 10
 
-python train.py --model rtsrgan --load_weights --batch_size 32 --learning_rate 1e-2 --lr_decay_epochs 10 --num_epochs 100 --steps_per_epochs 100 --epochs_per_save 5
-
-#4x
-python train.py --model rtsrgan --train_dataset_info_path datasets/train/dataset_info.txt --train_dataset_path datasets/train/dataset.tfrecords --valid_dataset_info_path datasets/test/dataset_info.txt --valid_dataset_path datasets/test/dataset.tfrecords --batch_size 64 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 100 --steps_per_epochs 100 --epochs_per_save 5
-
-
+# G_ERTSRGAN Train 3x
+python train.py --model g_ertsrgan --train_dataset_info_path datasets/train/3x/dataset_info.txt --train_dataset_path datasets/train/3x/dataset.tfrecords --valid_dataset_info_path datasets/test/3x/dataset_info.txt --valid_dataset_path datasets/test/3x/dataset.tfrecords --batch_size 32 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5
+sleep 10
+# G_ERTSRGAN Train 3x, load weights, lr 1e-4
+python train.py --model g_ertsrgan --train_dataset_info_path datasets/train/3x/dataset_info.txt --train_dataset_path datasets/train/3x/dataset.tfrecords --valid_dataset_info_path datasets/test/3x/dataset_info.txt --valid_dataset_path datasets/test/3x/dataset.tfrecords --batch_size 32 --learning_rate 1e-4 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --load_weights
 sleep 10
 
-python train.py --model ertsrgan --load_weights --batch_size 32 --learning_rate 1e-2 --lr_decay_epochs 10 --num_epochs 100 --steps_per_epochs 100 --epochs_per_save 5
 
-#4x
-python train.py --model ertsrgan --train_dataset_info_path datasets/train/dataset_info.txt --train_dataset_path datasets/train/dataset.tfrecords --valid_dataset_info_path datasets/test/dataset_info.txt --valid_dataset_path datasets/test/dataset.tfrecords --batch_size 64 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 100 --steps_per_epochs 100 --epochs_per_save 5
+# G_ERTSRGAN 4x, transfer learning, lr 10e-3 
+python train.py --model g_ertsrgan --train_dataset_info_path datasets/train/4x/dataset_info.txt --train_dataset_path datasets/train/4x/dataset.tfrecords --valid_dataset_info_path datasets/test/4x/dataset_info.txt --valid_dataset_path datasets/test/4x/dataset.tfrecords --batch_size 32 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --transfer_learning
+sleep 10
+# G_ERTSRGAN 4x, load weights, lr 10e-4
+python train.py --model g_ertsrgan --train_dataset_info_path datasets/train/4x/dataset_info.txt --train_dataset_path datasets/train/4x/dataset.tfrecords --valid_dataset_info_path datasets/test/4x/dataset_info.txt --valid_dataset_path datasets/test/4x/dataset.tfrecords --batch_size 32 --learning_rate 1e-4 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --load_weights
+sleep 10
+
+# Parou aqui
+# RTSRGAN Train 2x
+python train.py --model rtsrgan --train_dataset_info_path datasets/train/2x/dataset_info.txt --train_dataset_path datasets/train/2x/dataset.tfrecords --valid_dataset_info_path datasets/test/2x/dataset_info.txt --valid_dataset_path datasets/test/2x/dataset.tfrecords --batch_size 32 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --load_weights --trainable_layer final
+sleep 10
+# RTSRGAN Train 2x, load weights, lr 1e-4
+python train.py --model rtsrgan --train_dataset_info_path datasets/train/2x/dataset_info.txt --train_dataset_path datasets/train/2x/dataset.tfrecords --valid_dataset_info_path datasets/test/2x/dataset_info.txt --valid_dataset_path datasets/test/2x/dataset.tfrecords --batch_size 32 --learning_rate 1e-4 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --load_weights --trainable_layer final
+sleep 10
+# RTSRGAN 4x, transfer learning, lr 10e-3 
+python train.py --model rtsrgan --train_dataset_info_path datasets/train/4x/dataset_info.txt --train_dataset_path datasets/train/4x/dataset.tfrecords --valid_dataset_info_path datasets/test/4x/dataset_info.txt --valid_dataset_path datasets/test/4x/dataset.tfrecords --batch_size 32 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --load_weights --trainable_layer final
+sleep 10
+# RTSRGAN 4x, load weights, lr 10e-4
+python train.py --model rtsrgan --train_dataset_info_path datasets/train/4x/dataset_info.txt --train_dataset_path datasets/train/4x/dataset.tfrecords --valid_dataset_info_path datasets/test/4x/dataset_info.txt --valid_dataset_path datasets/test/4x/dataset.tfrecords --batch_size 32 --learning_rate 1e-4 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --load_weights --trainable_layer final
+sleep 10
+
+# ERTSRGAN Train 2x
+python train.py --model ertsrgan --train_dataset_info_path datasets/train/2x/dataset_info.txt --train_dataset_path datasets/train/2x/dataset.tfrecords --valid_dataset_info_path datasets/test/2x/dataset_info.txt --valid_dataset_path datasets/test/2x/dataset.tfrecords --batch_size 32 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5
+sleep 10
+# ERTSRGAN Train 2x, load weights, lr 1e-4
+python train.py --model ertsrgan --train_dataset_info_path datasets/train/2x/dataset_info.txt --train_dataset_path datasets/train/2x/dataset.tfrecords --valid_dataset_info_path datasets/test/2x/dataset_info.txt --valid_dataset_path datasets/test/2x/dataset.tfrecords --batch_size 32 --learning_rate 1e-4 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --load_weights
+sleep 10
+# ERTSRGAN Train 3x
+python train.py --model ertsrgan --train_dataset_info_path datasets/train/3x/dataset_info.txt --train_dataset_path datasets/train/3x/dataset.tfrecords --valid_dataset_info_path datasets/test/3x/dataset_info.txt --valid_dataset_path datasets/test/3x/dataset.tfrecords --batch_size 32 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5
+sleep 10
+# ERTSRGAN Train 3x, load weights, lr 1e-4
+python train.py --model ertsrgan --train_dataset_info_path datasets/train/3x/dataset_info.txt --train_dataset_path datasets/train/3x/dataset.tfrecords --valid_dataset_info_path datasets/test/3x/dataset_info.txt --valid_dataset_path datasets/test/3x/dataset.tfrecords --batch_size 32 --learning_rate 1e-4 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --load_weights
+sleep 10
+
+# ERTSRGAN 4x, transfer learning, lr 10e-3 
+python train.py --model ertsrgan --train_dataset_info_path datasets/train/4x/dataset_info.txt --train_dataset_path datasets/train/4x/dataset.tfrecords --valid_dataset_info_path datasets/test/4x/dataset_info.txt --valid_dataset_path datasets/test/4x/dataset.tfrecords --batch_size 32 --learning_rate 1e-3 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --transfer_learning
+sleep 10
+# ERTSRGAN 4x, load weights, lr 10e-4
+python train.py --model ertsrgan --train_dataset_info_path datasets/train/4x/dataset_info.txt --train_dataset_path datasets/train/4x/dataset.tfrecords --valid_dataset_info_path datasets/test/4x/dataset_info.txt --valid_dataset_path datasets/test/4x/dataset.tfrecords --batch_size 32 --learning_rate 1e-4 --lr_decay_epochs 20 --num_epochs 10 --steps_per_epochs 100 --epochs_per_save 5 --load_weights
